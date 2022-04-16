9c953998761e413e20d55f7f0bc1b3bb36a86a8c
janhavi <janashinde394@gmail.com>
2022-04-16 21:57:23 +1000

    fixed bug of not rendering last quix question

--- code/Phase2/my-app/src/Play.js
+++ code/Phase2/my-app/src/Play.js
@@ -8,17 +8,26 @@ function Play({ questions }) {
     
 
 const currentQ = questions[questionIndex];
-const valueLength = questions.length - 1; // 2
-console.log(valueLength);
+const arrayLength = questions.length -1 ; // 4
+
+console.log("currentQ's index:", questionIndex); //5 
 
 function handleClick(){
-    console.log("questionIndex was:", questionIndex)
-    setQuestionIndex(questionIndex + 1);
-    console.log("questionIndex now increased to:", questionIndex);
+    if (questionIndex < arrayLength) {
+        console.log("questionIndex was:", questionIndex);
+        setQuestionIndex(questionIndex + 1);
+        console.log("questionIndex now increased to:", questionIndex);
+        console.log(questionIndex, "out of", arrayLength);
+    } else if (questionIndex === arrayLength) {
+         console.log("questionIndex has reached", questionIndex, "out of ", arrayLength);
+         setTernary(true);
+        }
 }
 
-const ternary = () => (questionIndex < valueLength); // valueLength = 2
+// const ternary = () => (questionIndex === arrayLength); // arrayLength = 2
 // const ternary = questions[-1]
+
+const  [ ternary, setTernary ] = useState(false)
 
 
 const options = currentQ.options.map(option => (
@@ -28,26 +37,39 @@ const button = <button onClick={handleClick}>Submit</button>
 
 const startOver = <button onClick={handleStartOver}>Start Over </button>
 function handleStartOver () {
-    setQuestionIndex(0)
+    setQuestionIndex(0);
+    setTernary(false)
 }
 
 
     return (
 
         <div className="QuestionCard">
-{ternary() ? <div className="QuestionCard">
-                <h3> Questions: {currentQ.question} </h3> 
-                {options} {button} 
-            </div> 
-            : <>
+{ternary? <>
             <h2> GAME OVER. YOUR SCORE IS: X </h2> 
             {startOver}
-            </> }
+            </> 
+            :  <div className="QuestionCard">
+            <h3> Questions: {currentQ.question} </h3> 
+            {options} {button} 
+        </div>}
             
         </div>
-
 
     )
 }
 
 export default Play;
+
+
+// {/* <div className="QuestionCard">
+// {currentQ ? <div className="QuestionCard">
+//                 <h3> Questions: {currentQ.question} </h3> 
+//                 {options} {button} 
+//             </div> 
+//             : <>
+//             <h2> GAME OVER. YOUR SCORE IS: X </h2> 
+//             {startOver}
+//             </> }
+            
+//         </div> */}
